ca4159b00312ab5e3ae9e5d2fc64a9f542f60b36
Sebastian <spizzuto@uri.edu>
2022-01-31 18:07:18 -0500

    updated

---
+++ fighter.py
@@ -0,0 +1,66 @@
+import pygame
+
+
+# Fighter Class
+class Fighter:
+    def __init__(self, screen, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        # Import images from directory into a list for idel animation
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0  # 0:idle, 1:attack, 2:hurt, 3:dead
+        self.update_time = pygame.time.get_ticks()
+
+        # load idle images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        # load attack images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        # load hurt images
+        temp_list = []
+        for i in range(2):
+            img = pygame.image.load(f'img/{self.name}/Hurt/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        # load death images
+        temp_list = []
+        for i in range(10):
+            img = pygame.image.load(f'img/{self.name}/Death/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        animation_cooldown = 100
+        # handle animation
+        # update image
+        self.image = self.animation_list[self.action][self.frame_index]
+        # checks if enough time has passed since last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        # if animation has run out, reset to the start of the animation
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
---
+++ main.py
@@ -0,0 +1,89 @@
+from pygame import *
+from fighter import *
+
+pygame.init()
+mixer.init()
+
+mixer.music.load('Adventure.mp3')
+mixer.music.set_volume(1)
+mixer.music.play()
+
+clock = pygame.time.Clock()
+fps = 60
+
+# game window
+bottom_panel = 150
+screen_width = 800
+screen_height = 400 + bottom_panel
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Battle')
+
+# define fonts
+font = pygame.font.SysFont('Times New Roman', 26)
+
+# define colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+# load images
+# background image
+background_img = pygame.image.load('img/Background/background.png').convert_alpha()
+# panel image
+panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
+
+
+# function to draw text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
+# function for drawing the background
+def draw_bg():
+    screen.blit(background_img, (0, 0))
+
+
+# Function for drawing Panel
+def draw_panel():
+    # draw panel rectangle
+    screen.blit(panel_img, (0, screen_height - bottom_panel))
+    # show knights stats
+    draw_text(f'{knight.name} HP: {knight.hp}', font, blue, 100, screen_height - bottom_panel + 10)
+    for count, i in enumerate(bandit_list):
+        draw_text(f'{i.name} HP: {i.hp}', font, red, 550, (screen_height - bottom_panel + 10) + count * 60)
+
+
+knight = Fighter(screen, 200, 260, 'Knight', 30, 10, 3)
+bandit1 = Fighter(screen, 550, 270, 'Bandit', 20, 6, 1)
+bandit2 = Fighter(screen, 700, 270, 'Bandit', 20, 6, 1)
+
+bandit_list = []
+bandit_list.append(bandit1)
+bandit_list.append(bandit2)
+
+running = True
+while running:
+    clock.tick(fps)
+
+    # draw background
+    draw_bg()
+
+    # draw panel
+    draw_panel()
+
+    # draw fighters
+    knight.update()
+    knight.draw(screen)
+    for bandit in bandit_list:
+        bandit.update()
+        bandit.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+
+pygame.quit()
